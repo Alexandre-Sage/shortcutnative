35abf8395ca09783858b5545ff9394ad5858a47b
ChorizoSurfista56 <chorizosurfista@gmail.com>
2021-11-10 16:39:30 +0100

    front-end-basique-home

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button, Image, TouchableOpacity} from 'react-native';
+import {Component} from "react"
+
+
+export default class HomeScreen extends Component {
+   constructor(props){
+      super(props)
+      this.state={
+         importationApi: [],
+      }
+
+      } componentDidMount(){
+      
+
+         } render(){
+              return (
+                  <View style={styles.mainContainerHome}>
+
+                     <Text style={styles.homeTitle}>Rechercher par: </Text>
+
+                     <View>
+                        <TouchableOpacity style={styles.buttonTouch}><Text style={styles.touchableText}>Catégories</Text></TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonTouch}><Text style={styles.touchableText}>Logiciels</Text></TouchableOpacity>
+                     </View>
+
+                     <Text style={styles.homeTitle}>Ou Bien: </Text>
+
+                     <View>
+                        <TouchableOpacity style={styles.buttonTouch}><Text style={styles.touchableText}>Ajouter un raccourcis</Text></TouchableOpacity>
+                     </View>
+                  </View>
+
+           );
+         }
+   };
+
+
+const styles = StyleSheet.create({
+   mainContainerHome:{
+      alignItems: "center",
+      justifyContent: "center",
+      height: 300,
+   },
+   homeTitle:{
+      fontSize: 20,
+      fontWeight: "bold",
+   },
+   buttonTouch:{
+      alignItems: 'center',
+      justifyContent: "center",
+      height: 50,
+      width: 200,
+      backgroundColor: "blue",
+      margin: 15,
+   },
+   touchableText:{
+      color: "white",
+   },
+
+  },
+);
+
+/*const styles = StyleSheet.create({
+
+
+  },
+);*/
