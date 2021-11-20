b2dfbc37643f711024d188a4acd6badd08f81343
Alexandre <chorizosurffista@gmail.com>
2021-11-20 15:17:19 +0100

    software a enlever debut test

--- src/screens/ShortcutScreen.jsx
+++ src/screens/ShortcutScreen.jsx
@@ -7,13 +7,34 @@ this.setState({choosenShortcut: shortcut})
 console.log(this.state.choosenShortcut);*/
 
 export default class ShortcutScreen extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            shortcutDetails: this.props.route.params.shortcut
+        }
 
-    render(){
+
+    } render(){
+        console.log(this.state.shortcutDetails);
         const shortcut= this.props.route.params.shortcut
+        const categoriesJsx= shortcut.categories.map((category)=>(
+            <Text key={category.id}>{category.name}</Text>
+        ))
+        const softwareJsx= shortcut.software.map((software)=>(
+            <Text key={software.id}>{software.title}</Text>
+        ))
         return(
             <View>
                 <Text>{shortcut.title}</Text>
+                <Text>{shortcut.context}</Text>
+                <Text>{shortcut.description}</Text>
+                <Text>{shortcut.linux}</Text>
+                <Text>{shortcut.macos}</Text>
+                <Text>{shortcut.windows}</Text>
+                {categoriesJsx}
+                {softwareJsx}
+
             </View>
-    )
+        )
     }
 };
