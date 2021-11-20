1d3d253ece72c2a6346da2b0155473c0a0c75bf9
Alexandre <chorizosurffista@gmail.com>
2021-11-20 16:56:04 +0100

    Page shortcut bien avancer

--- src/screens/ShortcutScreen.jsx
+++ src/screens/ShortcutScreen.jsx
@@ -2,39 +2,59 @@ import React from 'react';
 import {StyleSheet, Text, View, Button, Image, Picker, TouchableOpacity, ScrollView} from 'react-native';
 import {Component} from "react";
 
-/*const shortcut=this.props.navigation.getParams("shortcut");
-this.setState({choosenShortcut: shortcut})
-console.log(this.state.choosenShortcut);*/
-
 export default class ShortcutScreen extends Component{
     constructor(props){
         super(props)
         this.state={
-            shortcutDetails: this.props.route.params.shortcut
+            shortcutDetails: [this.props.route.params.shortcut]
         }
 
 
     } render(){
         console.log(this.state.shortcutDetails);
+        const DetailsJsx= this.state.shortcutDetails.map((shortcut, key)=>(
+            <View style={styles.mainContainerDetails} key={key}>
+            {
+
+                Object.entries(shortcut).slice(3,9).map(([title, detail, key])=>(
+                <View style={styles.row} key={key}>
+                    <Text style={styles.title}>{title}</Text>
+                    <Text style={styles.detail}>{detail}</Text>
+                </View>
+            ))}
+            </View>
+        ))
+
         const shortcut= this.props.route.params.shortcut
-        const categoriesJsx= shortcut.categories.map((category)=>(
+        const categoriesJsx= this.state.shortcutDetails[0].categories.map((category)=>(
             <Text key={category.id}>{category.name}</Text>
         ))
-        const softwareJsx= shortcut.software.map((software)=>(
-            <Text key={software.id}>{software.title}</Text>
-        ))
+        const softwareJsx= <Text>{this.state.shortcutDetails[0].software.name}</Text>
         return(
-            <View>
-                <Text>{shortcut.title}</Text>
-                <Text>{shortcut.context}</Text>
-                <Text>{shortcut.description}</Text>
-                <Text>{shortcut.linux}</Text>
-                <Text>{shortcut.macos}</Text>
-                <Text>{shortcut.windows}</Text>
+            <ScrollView style={styles.mainContainer}>
+                {DetailsJsx}
                 {categoriesJsx}
                 {softwareJsx}
-
-            </View>
+            </ScrollView>
         )
     }
 };
+
+const styles = StyleSheet.create({
+    mainContainer:{
+        height: 1500,
+
+        alignContent: 'space-around'
+    },
+    row:{
+        marginTop: 20,
+        borderWidth: 2
+    },
+    title:{
+        fontSize: 25,
+        textAlign: 'center'
+    },
+    detail:{
+        fontSize: 18,
+    }
+})
